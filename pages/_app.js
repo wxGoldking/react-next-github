98435da17f66ed544f844ab26e851624931265b5
wx <18729177374@163.com>
2020-11-16 22:31:18 +0800

    getInitialProps获取数据,自定义app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
 import 'antd/dist/antd.css'
 import './styles.css';
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Component {...pageProps} />
+  </>
+}
+
+MyApp.getInitialProps = async ({Component}) => {
+  let pageProps = {}
+  if(Component.getInitialProps) {
+    pageProps = await Component.getInitialProps();
+  }
+  return {
+    pageProps
+  };
 }
--- pages/a.jsx
+++ pages/a.jsx
@@ -1,11 +1,18 @@
 import { useRouter } from 'next/router'
 
-const A = () =>{
+const A = ({a, b, state}) =>{
   const router = useRouter();
   const {aaa} = router.query;
   return <h2>
-    A Page {aaa}
+    A Page {aaa} {a} {b}
   </h2>
+}
+
+A.getInitialProps = () => {
+  return {
+    a: 1,
+    b: 2
+  }
 }
 
 export default A;
